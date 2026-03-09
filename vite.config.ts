038379a69e1a02e6f41a3a2dfc952294308fffaf
keniquavasquez <96038071+keniquavasquez@users.noreply.github.com>
2026-03-08 20:45:49 -0400

    Add base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src',
+    base: '/Travel-Blog/',
+    plugins: [react()],
+    resolve: {
+          alias: {
+                  '@': '/src',
+          },
     },
-  },
 });
